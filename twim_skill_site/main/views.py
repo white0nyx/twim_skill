3e60185be72a1f96b35d0e96939cfae047947737
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-08-05 13:13:09 +0300

    Создана начальная страничка профиля

--- twim_skill_site/main/views.py
+++ twim_skill_site/main/views.py
@@ -1,11 +1,41 @@
+from allauth.socialaccount.models import SocialAccount
+from django.contrib.auth.models import User
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class MainPage(ListView):
     """Класс представления главной страницы"""
 
     def get(self, request, *args, **kwargs):
+        """Обработка get-запроса"""
+
         context = {}
 
         return render(request, 'main/main.html', context)
+
+
+class ProfilePage(DetailView):
+    """Класс представления страницы профиля пользователя"""
+    model = SocialAccount
+    template_name = 'main/profile.html'
+
+    def get(self, request: WSGIRequest, *args, **kwargs):
+        """Обработка get-запроса"""
+        user_data = SocialAccount.objects.filter(user=request.user)
+
+        # Проверка на то, является ли пользователь админом
+        if user_data:
+            user_data = user_data[0]
+        else:
+            user_data = {
+                'extra_data': {
+                    'avatarfull': '',
+                    'personaname': request.user,
+                    'profileurl': '',
+                }
+            }
+        context = {'user_data': user_data}
+        return render(request, 'main/profile.html', context)
+
--- twim_skill_site/twim_skill_site/urls.py
+++ twim_skill_site/twim_skill_site/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from main.views import MainPage
+from main.views import MainPage, ProfilePage
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', MainPage.as_view(), name='main')
+    path('', MainPage.as_view(), name='main'),
+    path('profile/', ProfilePage.as_view(), name='profile'),
 ]
 
 urlpatterns += [
