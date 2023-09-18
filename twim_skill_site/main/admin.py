7ba7de65a697588c8ee882d8d99fb698673e700d
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-09-18 15:38:39 +0300

    Рефакторинг. Удалён лишний код.

--- twim_skill_site/main/admin.py
+++ twim_skill_site/main/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('id','nickname', 'id_role', 'registration_date', 'last_enter_date', 'steam_url', 'faciet_url')
-
 
 @admin.register(Lobby)
 class LobbyAdmin(admin.ModelAdmin):
@@ -12,11 +8,13 @@ class LobbyAdmin(admin.ModelAdmin):
     list_filter = ('deleted',)
     search_fields = ('id_leader', 'map')
 
+
 @admin.register(PlayerLobby)
 class PlayersLobbyAdmin(admin.ModelAdmin):
     list_display = ('id_lobby', 'id_user', 'team_id', 'in_lobby')
     list_filter = ('team_id', 'in_lobby')
     search_fields = ('id_lobby__slug', 'id_user__nickname')
+
 
 class PlayersLobby(admin.ModelAdmin):
     list_display = ('id_lobby', 'id_user', 'team_id', 'in_lobby')
