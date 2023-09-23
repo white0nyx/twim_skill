27eb4f9a3c0e3e0f4e051ad23bb2f9ecd5c1227d
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-09-23 13:58:23 +0300

    Созданы и добавлены в админку модели приложения games

--- twim_skill_site/games/admin.py
+++ twim_skill_site/games/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(GameMap)
+class GameMapAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(GameType)
+class GameTypeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(GameStatus)
+class GameStatusAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'lobby', 'map', 'game_type', 'status', 'date_start', 'date_end')
+    list_filter = ('lobby', 'map', 'game_type', 'status')
+    search_fields = ('lobby',)
+
+
+@admin.register(PlayerGameInfo)
+class PlayerGameInfoAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'game', 'user')
+    list_filter = ('game', 'user',)
+    search_fields = ('game', 'user')
+
+
+@admin.register(PlayerStatisticInGame)
+class PlayerStatisticInGameAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'game', 'user', 'accuracy', 'headshots_count', 'kills', 'deaths')
+    list_filter = ('game', 'user',)
+    search_fields = ('game', 'user')
