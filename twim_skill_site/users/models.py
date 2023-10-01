1c9b8c2f5b27a41c367fa4ed9a06d6a362618431
matvik <sherbaev.matvey@mail.ru>
2023-10-01 21:01:35 +0300

    добавлены verbose_name

--- twim_skill_site/users/models.py
+++ twim_skill_site/users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     """Изменённая модель пользователя"""
-    experience = models.IntegerField()
-    balance = models.IntegerField(null=True)
+    experience = models.IntegerField(verbose_name='Опыт')
+    balance = models.IntegerField(null=True, verbose_name='Баланс')
 
     def save(self, *args, **kwargs):
         if self.balance < 0:
@@ -14,12 +14,12 @@ class User(AbstractUser):
 
 class TypeOfOperations(models.Model):
     "Модель типа операций"
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
 
 class Operations(models.Model):
     "Модель операций пользователя"
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="operations")
-    amount = models.IntegerField()
-    type_operations = models.ForeignKey(TypeOfOperations, on_delete=models.CASCADE)
-    date = models.DateTimeField()
-    source = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="operations", verbose_name='Пользователь')
+    amount = models.IntegerField(verbose_name='Сумма')
+    type_operations = models.ForeignKey(TypeOfOperations, on_delete=models.CASCADE, verbose_name='Тип операции')
+    date = models.DateTimeField(verbose_name='Дата операции')
+    source = models.CharField(max_length=50, verbose_name='Источник операции')
