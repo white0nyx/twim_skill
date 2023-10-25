b1b3e0f5e92bd377c804e730204935079e744f8f
matvik <sherbaev.matvey@mail.ru>
2023-10-25 13:17:15 +0300

    добавлена связь с матчем в OperationsGame

--- twim_skill_site/users/models.py
+++ twim_skill_site/users/models.py
@@ -1,24 +1,28 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
 from django.db import models
+from games.models import Match
+
 
 class User(AbstractUser):
     """Изменённая модель пользователя"""
     experience = models.IntegerField(verbose_name='Опыт')
     balance = models.DecimalField(max_digits=8, decimal_places=2, null=True, verbose_name='Баланс')
- 
+
+
 class TypeOfOperations(models.Model):
-    "Модель типа операций"
+    """Модель типа операций"""
     name = models.CharField(max_length=50, verbose_name='Название')
 
+
 class Operations(models.Model):
-    "Модель операций пользователя"
+    """Модель операций пользователя"""
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="operations", verbose_name='Пользователь')
     amount = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Сумма')
     type_operations = models.ForeignKey(TypeOfOperations, on_delete=models.CASCADE, verbose_name='Тип операции')
     date = models.DateTimeField(auto_now=True, verbose_name='Дата операции')
 
-class OperationsGame(models.Model):
-    operation = models.ForeignKey(Operations, on_delete=models.CASCADE, related_name="operations_games", verbose_name="Операция")
-    game = models.IntegerField(verbose_name="Матч")
 
+class OperationsGame(models.Model):
+    """Модель операций связанных с матчем"""
+    operation = models.ForeignKey(Operations, on_delete=models.CASCADE, related_name="d", verbose_name="Операция")
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name="d", verbose_name="Матч")
