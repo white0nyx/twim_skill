b2b5feabebe0e4a333649ead051013bdaa64a935
matvik <sherbaev.matvey@mail.ru>
2023-10-25 13:03:28 +0300

    подправил модель Operations создал модель OperationsGame изменил поля на DecimalField на баланс

--- twim_skill_site/users/models.py
+++ twim_skill_site/users/models.py
@@ -5,21 +5,20 @@ from django.db import models
 class User(AbstractUser):
     """Изменённая модель пользователя"""
     experience = models.IntegerField(verbose_name='Опыт')
-    balance = models.IntegerField(null=True, verbose_name='Баланс')
-
-    def save(self, *args, **kwargs):
-        if self.balance < 0:
-            raise ValidationError("Баланс не может быть отрицательным.")
-        super().save(*args, **kwargs)
-
+    balance = models.DecimalField(max_digits=8, decimal_places=2, null=True, verbose_name='Баланс')
+ 
 class TypeOfOperations(models.Model):
     "Модель типа операций"
-    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    name = models.CharField(max_length=50, verbose_name='Название')
 
 class Operations(models.Model):
     "Модель операций пользователя"
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="operations", verbose_name='Пользователь')
-    amount = models.IntegerField(verbose_name='Сумма')
+    amount = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Сумма')
     type_operations = models.ForeignKey(TypeOfOperations, on_delete=models.CASCADE, verbose_name='Тип операции')
-    date = models.DateTimeField(verbose_name='Дата операции')
-    source = models.CharField(max_length=50, verbose_name='Источник операции')
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата операции')
+
+class OperationsGame(models.Model):
+    operation = models.ForeignKey(Operations, on_delete=models.CASCADE, related_name="operations_games", verbose_name="Операция")
+    game = models.IntegerField(verbose_name="Матч")
+
