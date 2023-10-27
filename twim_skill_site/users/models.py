a8a6a4c8dbb4f225d4b7191a84c82b488abe61a7
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-10-27 18:20:30 +0300

    В админку добавлено отображение моделей, связанных с операциями

--- twim_skill_site/users/models.py
+++ twim_skill_site/users/models.py
@@ -8,13 +8,14 @@ class User(AbstractUser):
     balance = models.DecimalField(max_digits=8, decimal_places=2, null=True, verbose_name='Баланс')
 
 
+# <-------------- Операции с балансом -------------->
 class TypeOfOperations(models.Model):
-    "Модель типа операций"
+    """Модель типа операций"""
     name = models.CharField(max_length=50, verbose_name='Название')
 
 
 class Operations(models.Model):
-    "Модель операций пользователя"
+    """Модель операций пользователя"""
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="operations", verbose_name='Пользователь')
     amount = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Сумма')
     type_operations = models.ForeignKey(TypeOfOperations, on_delete=models.CASCADE, verbose_name='Тип операции')
@@ -22,6 +23,7 @@ class Operations(models.Model):
 
 
 class OperationsGame(models.Model):
+    """Модель связи операции с игрой"""
     operation = models.ForeignKey(Operations, on_delete=models.CASCADE, related_name="operations_games",
                                   verbose_name="Операция")
     game = models.IntegerField(verbose_name="Матч")
