e550b316bc96906d89fbdb41c1dec187ad790587
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-10-27 23:13:42 +0300

    Выбор карты предоставляется в виде выпадающего списка

--- twim_skill_site/lobby/views.py
+++ twim_skill_site/lobby/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views import View
 
+from games.models import Map
 from lobby.models import *
 from lobby.services import *
 from users.services import get_steam_faceit_user_data
@@ -17,7 +18,11 @@ class CreateLobbyPage(View):
     @staticmethod
     def get(request) -> HttpResponse:
         """Обработчик get-запроса"""
-        return render(request, 'lobby/create_lobby.html')
+
+        context = {
+            'maps': Map.objects.all(),
+        }
+        return render(request, 'lobby/create_lobby.html', context)
 
     @staticmethod
     def post(request) -> HttpResponseRedirect:
@@ -27,11 +32,11 @@ class CreateLobbyPage(View):
         insufficient_balance = False
 
         if user.is_authenticated:
-            game_map = request.POST.get('map')
-            bet = Decimal(request.POST.get('bet', 0))
+            game_map = request.POST.get('maps')
+            bet = Decimal(request.POST.get('bet')) if request.POST.get('bet') else 0
             password_lobby = request.POST.get('password_lobby')
-            max_lvl_enter = request.POST.get('max_lvl_enter')
-            min_lvl_enter = request.POST.get('min_lvl_enter')
+            max_lvl_enter = request.POST.get('max_lvl_enter') if request.POST.get('max_lvl_enter') else 10
+            min_lvl_enter = request.POST.get('min_lvl_enter') if request.POST.get('min_lvl_enter') else 0
             slug = slugify(f"{game_map}-{user.id}-{timezone.now().strftime('%Y%m%d%H%M%S')}")
 
             if user.balance >= bet:
@@ -57,7 +62,12 @@ class CreateLobbyPage(View):
             else:
                 insufficient_balance = True
 
-        return render(request, 'create_lobby.html', {'insufficient_balance': insufficient_balance})
+        context = {
+            'insufficient_balance': insufficient_balance,
+            'maps': Map.objects.all(),
+        }
+
+        return render(request, 'lobby/create_lobby.html', context)
 
 
 class DetailLobbyPage(View):
