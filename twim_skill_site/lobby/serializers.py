1006fac3296f63658f65af5fff773138fa29bd8b
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2024-01-30 13:27:09 +0300

    Поля game_type и game_mode удалены из API-запроса к лобби

--- twim_skill_site/lobby/serializers.py
+++ twim_skill_site/lobby/serializers.py
@@ -6,4 +6,4 @@ from lobby.models import Lobby
 class LobbySerializer(serializers.ModelSerializer):
     class Meta:
         model = Lobby
-        fields = ('leader', 'map', 'game_type', 'game_mode', 'min_lvl_enter', 'max_lvl_enter', 'match')
+        fields = ('leader', 'map', 'min_lvl_enter', 'max_lvl_enter', 'match')
