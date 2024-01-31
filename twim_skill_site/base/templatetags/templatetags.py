71c0c9993b121e0fe231dfc174eabae9aaf81789
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2024-01-31 12:26:30 +0300

    Обработка кнопки игры частично переписана на API

--- twim_skill_site/base/templatetags/templatetags.py
+++ twim_skill_site/base/templatetags/templatetags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def remove_trailing_zeros(number):
     return ('%.15f' % number).rstrip('0').rstrip('.')
+
+
+@register.filter
+def iso_format(value):
+    return value.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
