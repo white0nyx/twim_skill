05333906a34a0d3d7054d6d18994367589f23701
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2024-02-06 13:24:00 +0300

    Исправлен файл admin.py

--- twim_skill_site/lobby/admin.py
+++ twim_skill_site/lobby/admin.py
@@ -4,14 +4,13 @@ from .models import *
 
 @admin.register(Lobby)
 class LobbyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'leader', 'map', 'bet', 'password_lobby', 'max_lvl_enter', 'slug')
+    list_display = ('id', 'leader', 'password_lobby', 'slug')
     search_fields = ('leader', 'map')
 
 
 @admin.register(PlayerLobby)
 class PlayersLobbyAdmin(admin.ModelAdmin):
-    list_display = ('lobby', 'user', 'team_id', 'in_lobby', 'time_enter')
-    list_filter = ('team_id', 'in_lobby', 'time_enter')
+    list_display = ('lobby', 'user', 'time_enter')
     search_fields = ('lobby__slug', 'user__nickname')
 
 
